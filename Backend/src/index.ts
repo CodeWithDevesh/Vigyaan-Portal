9687ab9c894e452e75de42cbdd274037083102b7
VibhorShukla-BHRT <shukla.vibhor495@gmail.com>
2025-03-11 00:36:56 +0530

    Auth & DB setup

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import connectDatabase from "./db/db";
+import authRouter from "./routes/auth";
 
 const app = express();
 const PORT=5000;
 app.use(cors());
 app.use(express.json());
 
+connectDatabase();
+
 app.get('/api/2025',(req:Request,res:Response)=>{
     res.send("SERVER RESPONDING");
 })
+app.use('/vigyaanportal/v1/auth',authRouter);
 app.listen(PORT,()=>{
     console.log("Server Running at port 5000");
 })
---
+++ Backend/src/models/models.ts
@@ -0,0 +1,178 @@
+import mongoose from "mongoose";
+
+//User Schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    branch: {
+      type: String,
+      enum: ["CSE", "IT", "ECE", "ME", "EE"],
+      required: true,
+    },
+    sem: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 8,
+    },
+    role: {
+      type: String,
+      enum: ["winner", "user"],
+      default: "user",
+    },
+    projects_created: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
+    ],
+    projects_assigned: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+      },
+    ],
+    verified: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
+
+//Project Schema
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    problem_statement: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    branch: {
+      type: String,
+      enum: ["CSE", "IT", "ECE", "ME", "EE"],
+      required: true,
+    },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    }, // Original Winner
+    assigned_to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+      validate: {
+        validator: function (
+          this: any,
+          value: mongoose.Types.ObjectId | null
+        ): boolean {
+          return this.status === "taken" ? value !== null : true;
+        },
+        message: "Assigned_to must be set when project status is 'taken'.",
+      },
+    }, // Current Owner
+    status: {
+      type: String,
+      enum: ["available", "taken"],
+      default: "available",
+    },
+    image: {
+      type: String,
+    }, // Image URL of project
+  },
+  { timestamps: true }
+);
+
+const requestSchema = new mongoose.Schema(
+  {
+    project_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
+    requested_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "denied"],
+      default: "pending",
+    },
+    request_date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+requestSchema.index({ project_id: 1, requested_by: 1 }, { unique: true });
+
+
+const notificationSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["request_sent", "request_approved", "request_denied"],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    sent_at: {
+      type: Date,
+      default: Date.now,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserOTPVerificationSchema = new mongoose.Schema({
+  userId: String,
+  otp: String,
+  createdAt: Date,
+  expiry: Date
+})
+
+const userModel = mongoose.model("Users",userSchema,"Users");
+const projectModel = mongoose.model("Projects",projectSchema,"Projects");
+const requestModel = mongoose.model("Requests",requestSchema,"Requests");
+const notificationModel = mongoose.model("Notifications",notificationSchema,"Notifications");
+const userOTPVerificationModel = mongoose.model("UserOTPVerification",UserOTPVerificationSchema,"UserOTPVerification");
+export{
+    userModel,
+    projectModel,
+    requestModel,
+    notificationModel,
+    userOTPVerificationModel
+};
